6b0d9fb96ecf29fc6e9136b4dbbe85a93471ff41
Raian Barbosa <raianpbstudio@gmail.com>
2025-01-11 12:23:16 -0300

    alow host

--- server/settings.py
+++ server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4yr-yd-u5c*fk%+o9ar=vhh=%8)*lpos#*#e3n&p=#iy1t&hj9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
